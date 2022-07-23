4b2d90bf7e75900f93974fb2bef7c10b63cb5bfd
413432643 <9022695+zdp12345@user.noreply.gitee.com>
2022-07-23 14:55:16 +0800

    'tst'

--- src/pages/Distribution.vue
+++ src/pages/Distribution.vue
@@ -1,35 +1,101 @@
 <template>
   <el-row :gutter="10" style="margin: 0; padding: 0; margin-top: 1vh">
     <el-col :span="8">
-      <chartpanel title="数量统计" style="height: 30vh"></chartpanel>
+      <chartpanel title="数量统计" style="height: 30vh">
+        <div class="today-item">
+          <span>运单数:</span>
+          <span
+            v-for="(item, index) in today.in"
+            :key="index"
+            class="number-item text_color_11"
+          >
+            {{ item }}
+          </span>
+        </div>
+
+        <div class="today-item">
+          <span>员工数:</span>
+          <span
+            v-for="(item, index) in today.end"
+            :key="index"
+            class="number-item text_color_12"
+          >
+            {{ item }}
+          </span>
+        </div>
+      </chartpanel>
     </el-col>
     <el-col :span="8">
-      <chartpanel title="类型分布" style="height: 30vh"></chartpanel>
+      <chartpanel title="类型分布" style="height: 30vh">
+        <v-chart
+          ref="typeChart"
+          style="min-height: 25vh"
+          :option="typeChartOption"
+        ></v-chart>
+      </chartpanel>
     </el-col>
     <el-col :span="8">
-      <chartpanel title="状态分布" style="height: 30vh"></chartpanel>
+      <chartpanel title="状态分布" style="height: 30vh">
+        <v-chart
+          ref="statusChart"
+          style="min-height: 25vh"
+          :option="statusChartOption"
+        ></v-chart>
+      </chartpanel>
     </el-col>
   </el-row>
   <el-row :gutter="10" style="margin: 0; padding: 0; margin-top: 1vh">
     <el-col :span="8">
-      <chartpanel title="数量走势" style="height: 30vh"></chartpanel>
+      <chartpanel title="数量走势" style="height: 30vh">
+        <v-chart
+          ref="trendChart"
+          style="min-height: 25vh"
+          :option="trendChartOption"
+        ></v-chart>
+      </chartpanel>
     </el-col>
     <el-col :span="8">
-      <chartpanel title="次数分布" style="height: 30vh"></chartpanel>
+      <chartpanel title="次数分布" style="height: 30vh">
+        <v-chart
+          ref="numberChart"
+          style="min-height: 25vh"
+          :option="numberChartOption"
+        ></v-chart>
+      </chartpanel>
     </el-col>
     <el-col :span="8">
-      <chartpanel title="人员类型" style="height: 30vh"></chartpanel>
+      <chartpanel title="人员类型" style="height: 30vh">
+        <v-chart
+          ref="peopleChart"
+          style="min-height: 25vh"
+          :option="peopleChartOption"
+        ></v-chart>
+      </chartpanel>
     </el-col>
   </el-row>
   <el-row :gutter="10" style="margin: 0; padding: 0; margin-top: 1vh">
     <el-col :span="8">
-      <chartpanel title="数量排行" style="height: 30vh"></chartpanel>
+      <chartpanel title="数量排行" style="height: 30vh">
+        <v-chart
+          ref="quantityChart"
+          style="min-height: 25vh"
+          :option="quantityChartOption"
+        ></v-chart>
+      </chartpanel>
     </el-col>
     <el-col :span="8">
-      <chartpanel title="配送时间" style="height: 30vh"></chartpanel>
+      <chartpanel title="配送时间" style="height: 30vh">
+         <v-chart
+          ref="timeChart"
+          style="min-height: 25vh"
+          :option="timeChartOption"
+        ></v-chart>
+      </chartpanel>
     </el-col>
     <el-col :span="8">
-      <chartpanel title="区域分布" style="height: 30vh"></chartpanel>
+      <chartpanel title="区域分布" style="height: 30vh">
+          <div id="warnChart" style="height: 25vh"></div>
+      </chartpanel>
     </el-col>
   </el-row>
 </template>
@@ -54,17 +120,39 @@ import "echarts-gl";
 
 const $echarts = echarts;
 
-// 初始化数量统计
+// 初始化数据统计
+const today = reactive({
+  in: "000000",
+  end: "000000",
+  run: "000000",
+});
+const initTodayData = () => {
+  let inCount = utils.random(1000000) + "";
+  while (inCount.length < 6) {
+    inCount = "0" + inCount;
+  }
+  today.in = inCount;
+
+  let endCount = utils.random(1000000) + "";
+  while (endCount.length < 6) {
+    endCount = "0" + endCount;
+  }
+  today.end = endCount;
+
+  let runCount = utils.random(1000000) + "";
+  while (runCount.length < 6) {
+    runCount = "0" + runCount;
+  }
+  today.run = runCount;
+};
+
+// 初始化次数
 let numberChart = ref();
 let numberChartOption = reactive({});
 let numberChartCategory = reactive([
-  "配送",
-  "人事",
-  "管理",
-  "运送",
-  "库管",
-  "仓库",
-  "客服",
+  "第一次配送",
+  "第二次配送",
+  "第三次配送",
 ]);
 let numberChartValues = reactive([]);
 const initNumberChart = () => {
@@ -74,24 +162,302 @@ const initNumberChart = () => {
       numberChartOption,
       numberChartCategory,
       numberChartValues,
-      "#FF5722"
+      "#409eff"
     );
   });
 };
 
-// 更新数量统计
+// 初始化状态
+let statusChart = ref();
+let statusChartOption = reactive({});
+let statusChartCategory = reactive([
+  "待配送",
+  "配送中",
+  "拒绝",
+  "无法联系",
+  "已签收",
+]);
+let statusChartValues = reactive([]);
+const initStatusChart = () => {
+  statusChartCategory.forEach((item, index) => {
+    statusChartValues.push(utils.random(100));
+    chartutils.initBarChart(
+      statusChartOption,
+      statusChartCategory,
+      statusChartValues,
+      "#b3e19d"
+    );
+  });
+};
 
+
+// 初始化数量
+let quantityChart = ref();
+let quantityChartOption = reactive({});
+let quantityChartCategory = reactive([
+  "张三",
+  "李四",
+  "小明",
+  "小项",
+  "小章",
+  "小力",
+  "大力",
+  "大地",
+]);
+let quantityChartValues = reactive([]);
+const initQuantityChart = () => {
+  quantityChartCategory.forEach((item, index) => {
+    quantityChartValues.push(utils.random(100));
+    chartutils.initBarChart(
+      quantityChartOption,
+      quantityChartCategory,
+      quantityChartValues,
+      "#e6a23c"
+    );
+  });
+};
+
+// 初始化状态
+let typeChart = ref();
+let typeChartOption = reactive({});
+let typeChartCategory = reactive([
+  "用户自提",
+  "送货上门",
+  "货运物流",
+  "货到付款",
+]);
+let typeChartValues = reactive([]);
+const initTypeChart = () => {
+  typeChartCategory.forEach((item, index) => {
+    typeChartValues.push(utils.random(100));
+    chartutils.initPieFullChart(
+      typeChartOption,
+      typeChartCategory,
+      typeChartValues
+    );
+  });
+};
+
+// 初始化人员
+let peopleChart = ref();
+let peopleChartOption = reactive({});
+let peopleChartCategory = reactive([
+  "配送员",
+  "档案员",
+  "司机",
+  "人事",
+  "主管",
+]);
+let peopleChartValues = reactive([]);
+const initPeopleChart = () => {
+  peopleChartCategory.forEach((item, index) => {
+    peopleChartValues.push(utils.random(100));
+    chartutils.initPieFullChart(
+      peopleChartOption,
+      peopleChartCategory,
+      peopleChartValues
+    );
+  });
+};
+
+// 初始化订单走势
+let trendChart = ref();
+let trendChartOption = reactive({});
+let trendChartTime = reactive([]);
+let trendChartValues = reactive([]);
+const initTrendChart = () => {
+  let date = new Date();
+  date.setDate(date.getDate() - 7);
+  for (
+    let i = new Date(date.getTime());
+    i.getTime() < new Date().getTime();
+    i.setDate(i.getDate() + 1)
+  ) {
+    trendChartTime.push(i.format("MM-dd"));
+    trendChartValues.push(utils.random(1000));
+  }
+  chartutils.initLineChart(
+    trendChartOption,
+    trendChartTime,
+    trendChartValues,
+    "#f56c6c"
+  );
+};
+
+// 初始化订单走势
+let timeChart = ref();
+let timeChartOption = reactive({});
+let timeChartTime = reactive([]);
+let timeChartValues = reactive([]);
+const initTimeChart = () => {
+for (let i = 0; i < 12; i++) {
+			timeChartTime.push((i+7)+':00');
+			timeChartValues.push(utils.random(100));
+		}
+  chartutils.initLineChart(
+    timeChartOption,
+    timeChartTime,
+    timeChartValues,
+    "#79bbff"
+  );
+};
+
+// 初始化投诉统计
+let warnChart = null;
+let warnChartCategory = reactive([
+  "上海",
+  "北京",
+  "新疆",
+  "杭州",
+  "内蒙古",
+  "哈尔滨",
+  "黑龙江",
+  "杭州",
+  "西藏",
+]);
+const initWarnChart = () => {
+  let datas = [];
+  for (let i = 0; i < 50; i++) {
+    let color =
+      "rgb(" +
+      Math.round(Math.random() * 255) +
+      "," +
+      Math.round(Math.random() * 255) +
+      "," +
+      Math.round(Math.random() * 255) +
+      ")";
+    datas.push({
+      name: warnChartCategory[i % warnChartCategory.length],
+      value: (Math.random(100) * 1000).toFixed(0),
+      textStyle: {
+        color: color,
+      },
+    });
+  }
+
+  let option = {
+    tooltip: {
+      show: true,
+      position: "top",
+      textStyle: {
+        fontSize: 14,
+      },
+    },
+    series: [
+      {
+        type: "wordCloud",
+        // 网格大小，各项之间间距
+        gridSize: 20,
+        // 形状 circle 圆，cardioid  心， diamond 菱形，
+        // triangle-forward 、triangle 三角，star五角星
+        shape: "circle",
+        // 字体大小范围
+        sizeRange: [12, 30],
+        // 文字旋转角度范围
+        rotationRange: [0, 90],
+        // 旋转步值
+        rotationStep: 90,
+        // 自定义图形
+        // maskImage: maskImage,
+        left: "center",
+        top: "0",
+        // 画布宽
+        width: "95%",
+        // 画布高
+        height: "95%",
+        // 是否渲染超出画布的文字
+        drawOutOfBound: false,
+        data: datas,
+      },
+    ],
+  };
+  // 使用刚指定的配置项和数据显示图表。
+  warnChart = $echarts.init(document.getElementById("warnChart"));
+  warnChart.setOption(option);
+};
+
+
+
+const initCharts = () => {
+  initTodayData();
+  initNumberChart()
+  initStatusChart()
+  initQuantityChart()
+  initTypeChart()
+  initPeopleChart()
+  initTrendChart()
+  initTimeChart()
+  initWarnChart()
+};
+
+
+// 更新次数
 const updateNumberChart = () => {
   numberChartValues.forEach((item, index) => {
     numberChartValues[index] = utils.random(100);
   });
 };
 
-let mapChart = null;
-const initCharts = () => {
-  initNumberChart();
-  mapChart = chartutils.initMapChart("实时物流信息", "mapChart");
+// 更新状态
+const updateStatusChart = () => {
+  statusChartValues.forEach((item, index) => {
+    statusChartValues[index] = utils.random(100);
+  });
 };
+
+// 更新数量
+const updateQuantityChart = () => {
+  quantityChartValues.forEach((item, index) => {
+    quantityChartValues[index] = utils.random(100);
+  });
+};
+
+// 更新状态
+const updateTypeChart = () => {
+  typeChartOption.series[0].data.forEach((item, index) => {
+    item.value = utils.random(100);
+  });
+};
+
+// 更新人员
+const updatePeopleChart = () => {
+  peopleChartOption.series[0].data.forEach((item, index) => {
+    item.value = utils.random(100);
+  });
+};
+
+// 更新订单走势
+const updateTrendChart = () => {
+  trendChartValues.forEach((item, index) => {
+    trendChartValues[index] = utils.random(1000);
+  });
+};
+
+// 更新订单走势
+const updateTimeChart = () => {
+  timeChartValues.forEach((item, index) => {
+     timeChartValues[index] = utils.random(1000);
+  });
+};
+
+// 更新投诉统计
+const updateWarnChart = () => {
+  let warnChartOption = warnChart.getOption();
+  warnChartOption.series[0].data.forEach((item, index) => {
+    let color =
+      "rgb(" +
+      Math.round(Math.random() * 255) +
+      "," +
+      Math.round(Math.random() * 255) +
+      "," +
+      Math.round(Math.random() * 255) +
+      ")";
+    item.value = (Math.random(100) * 1000).toFixed(0);
+    item.textStyle.color = color;
+  });
+  warnChart.setOption(warnChartOption);
+};
+
 
 // 数据刷新
 let timer = null;
@@ -106,7 +472,15 @@ const startRefreshChart = () => {
   });
 
   timer = setInterval(function () {
-    updateNumberChart();
+    initTodayData();
+    updateNumberChart()
+    updateStatusChart()
+    updateQuantityChart()
+    updateTypeChart()
+    updatePeopleChart()
+    updateTrendChart()
+    updateTimeChart()
+    updateWarnChart()
   }, refreshtime);
 };
 
@@ -114,8 +488,13 @@ onMounted(() => {
   initCharts();
   startRefreshChart();
   window.onresize = () => {
-    mapChart && mapChart.resize();
     numberChart && numberChart.value.resize();
+    statusChart && statusChart.value.resize();
+    quantityChart && quantityChart.value.resize();
+    typeChart && typeChart.value.resize();
+    peopleChart && peopleChart.value.resize();
+    trendChart && trendChart.value.resize();
+    timeChart && timeChart.value.resize();
   };
 });
 
@@ -124,4 +503,22 @@ onBeforeUnmount(() => {
 });
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+  .today-item {
+  display: flex;
+  align-items: center;
+  height: 10vh;
+  margin-left: 2vh;
+  margin-top: 1vh;
+  .number-item {
+    background: #ffffff22;
+    font-size: 4rem;
+    width: 8vh; 
+    height: 8vh;
+    line-height: 8vh;
+    text-align: center;
+    margin-left: 1vh;
+    border-radius: 0.4vh;
+  }
+}
+</style>
--- src/pages/Order.vue
+++ src/pages/Order.vue
@@ -209,6 +209,340 @@ const initOrderChart = () => {
   );
 };
 
+// 初始化地图
+let mapChart = null;
+const initMapChart = (title, ele) => {
+  let chinaGeoCoordMap = {
+    黑龙江: [127.9688, 45.368],
+    内蒙古: [110.3467, 41.4899],
+    吉林: [125.8154, 44.2584],
+    北京市: [116.4551, 40.2539],
+    辽宁: [123.1238, 42.1216],
+    河北: [114.4995, 38.1006],
+    天津: [117.4219, 39.4189],
+    山西: [112.3352, 37.9413],
+    陕西: [109.1162, 34.2004],
+    甘肃: [103.5901, 36.3043],
+    宁夏: [106.3586, 38.1775],
+    青海: [101.4038, 36.8207],
+    新疆: [87.9236, 43.5883],
+    西藏: [91.11, 29.97],
+    四川: [103.9526, 30.7617],
+    重庆: [108.384366, 30.439702],
+    山东: [117.1582, 36.8701],
+    河南: [113.4668, 34.6234],
+    江苏: [118.8062, 31.9208],
+    安徽: [117.29, 32.0581],
+    湖北: [114.3896, 30.6628],
+    浙江: [119.5313, 29.8773],
+    福建: [119.4543, 25.9222],
+    江西: [116.0046, 28.6633],
+    湖南: [113.0823, 28.2568],
+    贵州: [106.6992, 26.7682],
+    云南: [102.9199, 25.4663],
+    广东: [113.12244, 23.009505],
+    广西: [108.479, 23.1152],
+    海南: [110.3893, 19.8516],
+    上海: [121.4648, 31.2891],
+  };
+  let chinaDatas = [
+    [
+      {
+        name: "黑龙江",
+        value: 0,
+      },
+    ],
+    [
+      {
+        name: "内蒙古",
+        value: 0,
+      },
+    ],
+    [
+      {
+        name: "吉林",
+        value: 0,
+      },
+    ],
+    [
+      {
+        name: "辽宁",
+        value: 0,
+      },
+    ],
+    [
+      {
+        name: "河北",
+        value: 0,
+      },
+    ],
+    [
+      {
+        name: "天津",
+        value: 0,
+      },
+    ],
+    [
+      {
+        name: "山西",
+        value: 0,
+      },
+    ],
+    [
+      {
+        name: "陕西",
+        value: 0,
+      },
+    ],
+    [
+      {
+        name: "甘肃",
+        value: 0,
+      },
+    ],
+    [
+      {
+        name: "宁夏",
+        value: 0,
+      },
+    ],
+    [
+      {
+        name: "青海",
+        value: 0,
+      },
+    ],
+    [
+      {
+        name: "新疆",
+        value: 0,
+      },
+    ],
+    [
+      {
+        name: "西藏",
+        value: 0,
+      },
+    ],
+    [
+      {
+        name: "四川",
+        value: 0,
+      },
+    ],
+    [
+      {
+        name: "重庆",
+        value: 0,
+      },
+    ],
+    [
+      {
+        name: "山东",
+        value: 0,
+      },
+    ],
+    [
+      {
+        name: "河南",
+        value: 0,
+      },
+    ],
+    [
+      {
+        name: "江苏",
+        value: 0,
+      },
+    ],
+    [
+      {
+        name: "安徽",
+        value: 0,
+      },
+    ],
+    [
+      {
+        name: "湖北",
+        value: 0,
+      },
+    ],
+    [
+      {
+        name: "浙江",
+        value: 0,
+      },
+    ],
+    [
+      {
+        name: "福建",
+        value: 0,
+      },
+    ],
+    [
+      {
+        name: "江西",
+        value: 0,
+      },
+    ],
+    [
+      {
+        name: "湖南",
+        value: 0,
+      },
+    ],
+    [
+      {
+        name: "贵州",
+        value: 0,
+      },
+    ],
+    [
+      {
+        name: "广西",
+        value: 0,
+      },
+    ],
+    [
+      {
+        name: "海南",
+        value: 0,
+      },
+    ],
+    [
+      {
+        name: "北京市",
+        value: 1,
+      },
+    ],
+  ];
+
+  let points = [];
+  for (let i = 0; i < chinaDatas.length; i++) {
+    let dataItem = chinaDatas[i];
+    let fromCoord = chinaGeoCoordMap[dataItem[0].name];
+    points.push([fromCoord[0], fromCoord[1], 11]);
+    for (let j = 0; j < 10; j++) {
+      points.push([
+        fromCoord[0] + Math.random() * 10,
+        fromCoord[1] + Math.random() * 10,
+        11,
+      ]);
+    }
+  }
+
+  let option = {
+    title: chartutils.createChartTitle(title),
+    amap: {
+      rotateEnable: true,
+      pitchEnable: true,
+      pitch: 0,
+      // rotation: -45,
+      // 3D模式，无论你使用的是1.x版本还是2.x版本，都建议开启此项以获得更好的渲染体验
+      viewMode: '3D',
+      // 高德地图支持的初始化地图配置
+      // 高德地图初始中心经纬度
+      center: [105.436561, 38.998546],
+      // 高德地图初始缩放级别
+      zoom: 4,
+      // 是否开启resize
+      resizeEnable: true,
+      // 自定义地图样式主题
+      mapStyle: "amap://styles/darkblue",
+      // 移动过程中实时渲染 默认为true 如数据量较大 建议置为false
+      renderOnMoving: true,
+      // ECharts 图层的 zIndex 默认 2000
+      // 从 v1.9.0 起 此配置项已被弃用 请使用 `echartsLayerInteractive` 代替
+      echartsLayerZIndex: 2019,
+      // 设置 ECharts 图层是否可交互 默认为 true
+      // 设置为 false 可实现高德地图自身图层交互
+      // 此配置项从 v1.9.0 起开始支持
+      echartsLayerInteractive: true,
+      // 是否启用大数据模式 默认为 false
+      // 此配置项从 v1.9.0 起开始支持
+      largeMode: false,
+      // 说明：如果想要添加卫星、路网等图层
+      // 暂时先不要使用layers配置，因为存在Bug
+      // 建议使用amap.add的方式，使用方式参见最下方代码
+    },
+    series: [
+      {
+        type: "effectScatter",
+        zlevel: 5,
+        // 使用百度地图坐标系
+        coordinateSystem: "amap",
+        //设置图形 'circle', 'rect', 'roundRect', 'triangle', 'diamond', 'pin', 'arrow'
+        // symbol: 'image://http://localhost:3000/src/assets/video.png',
+        symbol: "circle",
+        // //标记的大小，可以设置成诸如 10 这样单一的数字，也可以用数组分开表示宽和高，例如 [20, 10] 表示标记宽为20，高为10
+        symbolSize: [20, 20],
+        itemStyle: {
+          normal: {
+            color: "#FF5252", //标志颜色
+          },
+        },
+        // 数据格式跟在 geo 坐标系上一样，每一项都是 [经度，纬度，数值大小，其它维度...]
+        data: [[116.436561, 39.897346, 11]],
+        rippleEffect: {
+          scale: 6,
+          brushType: "stroke",
+        },
+        hoverAnimation: true, //是否开启鼠标 hover 的提示动画效果。
+      },
+      {
+        type: "effectScatter",
+        zlevel: 3,
+        // 使用百度地图坐标系
+        coordinateSystem: "amap",
+        //设置图形 'circle', 'rect', 'roundRect', 'triangle', 'diamond', 'pin', 'arrow'
+        // symbol: 'image://http://localhost:3000/src/assets/video.png',
+        symbol: "circle",
+        // //标记的大小，可以设置成诸如 10 这样单一的数字，也可以用数组分开表示宽和高，例如 [20, 10] 表示标记宽为20，高为10
+        symbolSize: [10, 10],
+        itemStyle: {
+          normal: {
+            // color: '#fac858', //标志颜色
+            color: function (pama) {
+              return config.colors[pama.dataIndex % config.colors.length];
+            },
+          },
+        },
+        // 数据格式跟在 geo 坐标系上一样，每一项都是 [经度，纬度，数值大小，其它维度...]
+        data: points,
+        rippleEffect: {
+          scale: 6,
+          brushType: "stroke",
+        },
+        hoverAnimation: true, //是否开启鼠标 hover 的提示动画效果。
+      },
+    ],
+  };
+  // 使用刚指定的配置项和数据显示图表。
+  let mapchart = $echarts.init(document.getElementById(ele));
+  mapchart.setOption(option);
+
+  // 获取 ECharts 高德地图组件
+  var amapComponent = mapchart.getModel().getComponent("amap");
+  // 获取高德地图实例，使用高德地图自带的控件(需要在高德地图js API script标签手动引入)
+  var amap = amapComponent.getAMap();
+  // 添加控件
+  amap.addControl(new AMap.Scale());
+  amap.addControl(new AMap.ToolBar());
+  amap.addControl(new AMap.ControlBar());
+  // 禁用 ECharts 图层交互，从而使高德地图图层可以点击交互
+  amapComponent.setEChartsLayerInteractive(false);
+
+  return mapchart;
+};
+
+const initCharts = () => {
+  initStartChart();
+  initEndChart();
+  initOriginChart();
+  initTypeChart();
+  initOrderChart();
+  mapChart = initMapChart("实时物流信息", "mapChart");
+};
+
 // 更新发布地
 const updateStartChart = () => {
   startChartValues.forEach((item, index) => {
@@ -283,16 +617,6 @@ let orders = reactive([
     status: "已揽件",
   },
 ]);
-
-let mapChart = null;
-const initCharts = () => {
-  initStartChart();
-  initEndChart();
-  initOriginChart();
-  initTypeChart();
-  initOrderChart();
-  mapChart = chartutils.initMapChart("实时物流信息", "mapChart");
-};
 
 // 数据刷新
 let timer = null;
